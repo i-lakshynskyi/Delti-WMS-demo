97243d6aa5aff0f32e60697caedbc632b3342227
David Geller <davidgeller@MacBook-Pro-David.local>
2025-07-08 15:55:47 +0300

    Refact styles qr-scan

--- src/utils/zxingQrEanScanner/ZxingQrEanScannerStyles.js
+++ src/utils/zxingQrEanScanner/ZxingQrEanScannerStyles.js
@@ -1,8 +1,8 @@
-export const ZxingQRScannerContainer = 'relative w-full h-[300px] overflow-hidden flex items-center justify-center mb-[10px]';
-export const ZxingQRScannerWrap = 'border-[2px] border-dashed border-[#1f456e] rounded-xl p-1'
-export const ZxingQrOverlay = "absolute w-full h-full flex items-center justify-center bg-[#ffffff] rounded-xl z-[10] border-dashed border-[2px] border-[#1f456e]"
+export const ZxingQRScannerContainer = 'relative w-full h-[300px] overflow-hidden flex items-center justify-center mb-[10px] border-[2px] border-dashed border-[#8fa2b6] rounded-xl';
+export const ZxingQRScannerWrap = 'rounded-xl p-1'
+export const ZxingQrOverlay = "absolute w-full h-full flex items-center justify-center bg-[#ffffff] z-[10]"
 export const ZxingQrOverlayImg = 'w-50 h-50 opacity-50'
 
 
-// main blue color: #1f456e active: #142c4f hover: #17355a
+// main blue color: #1f456e active: #142c4f hover: #17355a disabled: #8fa2b6
 // main gray color: #f5f5f5
